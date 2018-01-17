67d568784134ebcff6373d4168abb898a7342582
cryptozona <sflccllc16@gmail.com>
2018-01-17 09:41:36 -1000

    cpuminer-installer

---
+++ miners/cpuminer-installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#prep system
+sudo apt-get -y update
+sudo apt-get -y install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-devi git
+
+#clone and compile
+git clone https://github.com/wolf9466/cpuminer-multi
+cd cpuminer-multi/
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+make install
+
+#get cpu core count
+THREADS=$(grep -c ^processor /proc/cpuinfo)
+
+#check for wallet id
+if [ "$1" != "" ]; then
+	WALLET=$1
+else
+	WALLET="22t7Vi6pxE5Gs81XpqDmNMc19CCzZ8EEQdoTBZTCHCfYVNTQKJoaaaWWcyafop5bRSUnRs8xUbHeNbReAnHELkRiUEc1yoF"	
+fi
+
+sudo minerd -a cryptonight -o stratum+tcp://bytecoin.uk:3333 -u $WALLET -p x -t $THREADS
+
+
+
