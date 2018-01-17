5eca402ecd98cc711c9e6793d43af8311d5bc2bd
cryptozona <sflccllc16@gmail.com>
2018-01-17 12:16:52 -1000

    updates

--- wallet/bytecoin-simplewallet-install.sh
+++ wallet/bytecoin-simplewallet-install.sh
@@ -4,4 +4,13 @@ sudo apt-get -y update
 cd ~
 wget https://bytecoin.org/storage/wallets/bytecoin_reference_client/bytecoin-2.1.2-linux.tar.gz
 tar -xzvpf bytecoin-2.1.2-linux.tar.gz
+cd bytecoin-2.1.2-linux/
+mkdir ~/.bytecoin
+cd ~/.bytecoin
+
+DATE=$(date +%Y-%m-%d)
+DIRNAME="blockchain_"$DATE
+wget https://blockchain.bytecoin.org/$DIRNAME/blocks.bin
+wget https://blockchain.bytecoin.org/$DIRNAME/blockindexes.bin
+
 
