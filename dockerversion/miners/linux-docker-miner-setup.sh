ab458358ff6af53abd63271ed3dd55b075ecaa08
cryptozona <sflccllc16@gmail.com>
2018-01-18 08:32:04 -1000

    update

---
+++ dockerversion/miners/linux-docker-miner-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#INSTALL DOCKER
+export DEBIAN_FRONTEND=noninteractive
+apt -y -o Acquire::ForceIPv4=true update
+apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+apt -y install apt-transport-https ca-certificates curl software-properties-common git
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt -y -o Acquire::ForceIPv4=true update
+apt -y install docker-ce
+systemctl start docker
+systemctl enable docker
+
+#pull ubuntu
+docker pull ubuntu
+
+#check for wallet id
+if [ "$1" != "" ]; then
+	WALLET=$1
+else
+	WALLET="22t7Vi6pxE5Gs81XpqDmNMc19CCzZ8EEQdoTBZTCHCfYVNTQKJoaaaWWcyafop5bRSUnRs8xUbHeNbReAnHELkRiUEc1yoF"	
+fi
+
+
